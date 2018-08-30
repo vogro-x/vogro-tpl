7e04d8bb7f0df5ca6bdaa851b41b3e7923f3d5be
xiebei1108 <xiebei1108@outlook.com>
2018-08-30 23:33:03 +0800

    finish parsing html template: data, list and if

--- tpl.hpp
+++ tpl.hpp
@@ -3,7 +3,13 @@
 
 #include <iostream>
 #include <string>
+#include <stack>
+#include <vector>
 #include <map>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
 
 class Template {
     private:
@@ -13,20 +19,171 @@ class Template {
     std::map<std::string,std::vector<std::map<std::string,std::string> > >maplistMap; //{% for i in data %} {{ i.item1 }} {{% endfor %}}
 
     public:
-    Template(std::string src):sourceString(src){};
-    void setString(std::string key,std::string val){
+
+    Template(std::string src): sourceString(src) {
+
+    }
+
+    void setString(std::string key, std::string val) {
         this->dataMap[key]=val;
     }
-    void setStringList(std::string key,std::vector<std::string>& strList){
+    void setStringList(std::string key, std::vector<std::string>& strList) {
         this->listMap[key]=strList;
     }
 
-    void setMapList(std::string key,std::vector<std::map<std::string,std::string> >& mapList){
+    void setMapList(std::string key, std::vector<std::map<std::string,std::string> >& mapList) {
         this->maplistMap[key]=mapList;
     }
 
-    void printData(){
-        for(auto it=dataMap.begin();it!=dataMap.end();it++){
+    std::string getString(const std::string &key) {
+        return this->dataMap[key];
+    }
+
+    std::vector<std::string> getListMap(const std::string &key) {
+        return this->listMap[key];
+    };
+
+    std::vector<std::map<std::string, std::string>> getMapListMap(const std::string &key) {
+        return this->maplistMap[key];
+    };
+
+    std::string fillTemplate(const std::string &html_template) {
+        auto tokens = getTokens(html_template);
+        return render(tokens, dataMap);
+    }
+
+    std::string render(std::vector<std::string> tokens, std::map<std::string, std::string> context) {
+        std::vector<std::string> stk;
+        std::vector<std::string> result;
+        auto is_if = false, is_for = false;
+        int times = 0, match_times = 0;
+
+        for (auto item : tokens) {
+            auto prefix = item.substr(0, 2);
+
+            if (prefix != "{{" && prefix != "{%") {
+                if (!is_for && !is_if) {
+                    result.push_back(item);
+                } else {
+                    stk.push_back(item);
+                }
+            } else if (prefix == "{{") {
+                if (!is_for && !is_if) {
+                    result.push_back(context[getLabel(item)]);
+                } else {
+                    stk.push_back(item);
+                }
+            } else if (prefix == "{%") {
+                auto expression = item.substr(2, item.size()-4);
+                std::istringstream iss(expression);
+                std::vector<std::string> tokens {std::istream_iterator<std::string>{iss},
+                                                 std::istream_iterator<std::string>{}};
+                if (tokens.front() == "if") {
+                    stk.push_back(item);
+                    if (!is_for) {
+                        is_if = true;
+                        ++times;
+                    }
+                } else if (tokens.front() == "for") {
+                    stk.push_back(item);
+                    if (!is_if) {
+                        is_for = true;
+                        ++times;
+                    }
+                }
+
+                if (tokens.front() == "endif") {
+                    stk.push_back(item);
+                    if (!is_for) {
+                        ++match_times;
+                    }
+                    if (match_times == times) {
+                        result.push_back(IfBlock(stk, context));
+                        stk.clear();
+                        is_if = false, is_for = false;
+                        times = 0, match_times = 0;
+                    }
+
+                } else if (tokens.front() == "endfor") {
+                    stk.push_back(item);
+                    if (!is_if) {
+                        ++match_times;
+                    }
+                    if (match_times == times) {
+                        result.push_back(ForBlock(stk, context));
+                        stk.clear();
+                        is_if = false, is_for = false;
+                        times = 0, match_times = 0;
+                    }
+                }
+            } else {
+
+            }
+        }
+
+        std::string result_str;
+        for (auto str : result) {
+            result_str += str;
+        }
+        return result_str;
+    }
+
+    bool IfEngine(const std::string &cond_str) {
+        if (getString(cond_str) == "true") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    std::string IfBlock(std::vector<std::string> stk, std::map<std::string, std::string> context) {
+        std::istringstream iss(stk.front());
+        std::vector<std::string> if_lst {std::istream_iterator<std::string>{iss},
+                                                 std::istream_iterator<std::string>{}};
+        auto cond = IfEngine(if_lst[2]);
+        if (!cond) {
+            return std::string();
+        } else {
+            stk.erase(stk.begin());
+            return render(stk, context);
+        }
+    }
+
+    std::string ForBlock(std::vector<std::string> stk, std::map<std::string, std::string> context) {
+        auto for_engine = getLabel(stk.front());
+        std::istringstream iss(for_engine);
+        std::vector<std::string> for_engine_lst {std::istream_iterator<std::string>{iss},
+                                         std::istream_iterator<std::string>{}};
+
+        if (for_engine_lst.size() != 4 || for_engine_lst[0] != "for" || for_engine_lst[2] != "in") {
+            throw std::exception();
+        }
+
+        auto iter_name = for_engine_lst[1];
+        auto for_obj = getListMap(for_engine_lst.back());
+        std::string result;
+
+        stk.erase(stk.begin());
+        stk.pop_back();
+        for (auto x : for_obj) {
+            context[iter_name] = x;
+            result += render(stk, context);
+        }
+        return result;
+    }
+
+    std::string getLabel(std::string &str) {
+        str = str.substr(2, str.size() - 4);
+        auto begin = str.find_first_not_of(' ');
+        auto end = str.find_last_not_of(' ');
+        if (begin == std::string::npos || end == std::string::npos) {
+            return "";
+        }
+        return str.substr(begin, end - begin + 1);
+    }
+
+    void printData() {
+        for(auto it = dataMap.begin(); it != dataMap.end(); it++) {
             std::cout<<it->first<<":"<<it->second<<std::endl;
         }
 
@@ -45,6 +202,32 @@ class Template {
                  }
             }
         }
+    }
+
+    std::vector<std::string> getTokens(const std::string &html) {
+        std::vector<std::string> result;
+        auto n = html.size();
+        auto last_begin = 0;
+
+        for (int i = 0; i < n; ++i) {
+            auto open = html.substr(i, 2);
+            if (open == "{{" || open == "{%") {
+                result.push_back(html.substr(last_begin, i - last_begin));
+
+                std::string close_str = (open == "{{") ? "}}" : "%}";
+                auto end = html.find(close_str, i);
+                if (end == std::string::npos) {
+//                    throw std::exception();
+                    return result;
+                } else {
+                    result.push_back(html.substr(i, end - i + 2));
+                    i = end + 1;
+                    last_begin = i + 1;
+                }
+            }
+        }
+        result.push_back(html.substr(last_begin + 1, n - last_begin));
+        return result;
     }
 };
 
--- tpl_test.cpp
+++ tpl_test.cpp
@@ -2,12 +2,14 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <regex>
 #include "tpl.hpp"
 
-int main (void){
+int main (void) {
     Template tpl("hello,wolrd!");
 
     tpl.setString("key","value");
+    tpl.setString("user_name", "xvvx");
 
     std::vector<std::string> v {"Item1","Item2","Item3"};
     tpl.setStringList("vec1",v);
@@ -16,8 +18,31 @@ int main (void){
 
     tpl.setMapList("maplist",vm);
 
-    tpl.printData();
+    std::string html("<p>Welcome, {{ user_name }}!</p>\n"
+                             "\n"
+                             "{% if show_yes %}\n"
+                             "    Your name: {{ user_name }}\n"
+                             "{% endif %}\n"
+                             " {% if show_no %}\n"
+                             "    Your name: {{ user_name }}\n"
+                             "{% endif %}\n"
+                             "\n"
+                             "<p>Fruits:</p>\n"
+                             "<ul>\n"
 
+                             "{% for product in test_list %}\n"
+                             "    <li>{{ product }}</li>\n"
+                             "{% endfor %}\n"
+                             "</ul>\n");
+    Template test(html);
+    test.setString("user_name", "xvvx");
+    test.setString("show_yes", "true");
+    test.setString("show_no", "false");
+
+    std::vector<std::string> test_vec {"test1", "test2", "test3"};
+    test.setStringList("test_list", test_vec);
+
+    std::cout << test.fillTemplate(html) << std::endl;
 
     return 0;
 }

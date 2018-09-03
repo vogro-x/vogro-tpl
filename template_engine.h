7d9180a07e958500665e02b279fbad62a31b9d55
xiebei1108 <xiebei1108@outlook.com>
2018-09-03 23:00:37 +0800

    add string processing functions

---
+++ template_engine.h
@@ -0,0 +1,57 @@
+//
+// Created by xvvx on 18-9-2.
+//
+
+#ifndef VOGRO_TPL_TEMPLATE_ENGINE_H
+
+#define VOGRO_TPL_TEMPLATE_ENGINE_H
+
+#include <string.h>
+
+#include <string>
+#include <vector>
+
+namespace vstring {
+
+    std::vector<std::string> split(const std::string &str, const std::string &delim = " \n\t") {
+        std::vector<std::string> result;
+        //todo
+
+        return result;
+    }
+
+    std::string join(const std::vector<std::string> &str, const std::string &seq) {
+
+    }
+
+    std::string strip(const std::string &str, const std::string &rm) {
+
+    }
+
+    std::string lstrip(const std::string &str, const std::string &rm) {
+
+    }
+
+    std::string rstring(const std::string &str, const std::string &rm) {
+
+    }
+
+    bool start_with(const std::string &str, const std::string &prefix) {
+
+    }
+
+    bool end_with(const std::string &str, const std::string &suffix) {
+
+    }
+
+    std::string upper(const std::string &str) {
+
+    }
+
+    std::string lower(const std::string &str) {
+
+    }
+
+};
+
+#endif //VOGRO_TPL_TEMPLATE_ENGINE_H
---
+++ test.cc
@@ -0,0 +1,60 @@
+//
+// Created by xvvx on 18-9-2.
+//
+
+#include "template_engine.h"
+
+#include <string>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+
+TEST(VstringTest, StringSplit) {
+    std::string test_str1 = "hello,world,!";
+    auto result1 = std::vector<std::string>{"hello", "world", "!"};
+    ASSERT_EQ(vstring::split(test_str1, ","), result1);
+
+    std::string test_str2 = "hello \tworld\n!\n";
+    auto result2 = std::vector<std::string>{"hello", "world", "!"};
+    ASSERT_EQ(vstring::split(test_str2), result2);
+}
+
+TEST(VstringTest, StringStrip) {
+
+}
+
+TEST(VstringTest, StringLstrip) {
+
+}
+
+TEST(VstringTest, StringRstrip) {
+
+}
+
+TEST(VstringTest, StringJoin) {
+
+}
+
+TEST(VstringTest, StringStartWith) {
+
+}
+
+TEST(VstringTest, StringEndWith) {
+
+}
+
+TEST(StringToolTEST, StringUpper) {
+    
+}
+
+TEST(VstringTest, StringLower) {
+    
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
